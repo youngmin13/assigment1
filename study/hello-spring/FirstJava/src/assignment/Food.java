351b60b69527d73b014ab3a9e37f6b3f4a0771f4
youngmin13 <54584489+youngmin13@users.noreply.github.com>
2023-06-01 19:26:53 +0900

    Update Food.java
    생성자 한개 추가

--- study/hello-spring/FirstJava/src/assignment/Food.java
+++ study/hello-spring/FirstJava/src/assignment/Food.java
@@ -2,16 +2,19 @@ package assignment;
 
 public class Food extends Menu {
 
-
     private String foodName;
     private String foodDesc;
     private Double foodPrice;
+
+    public Food (String foodName, String foodDesc) {
+        super(foodName, foodDesc);
+    };
 
     // 생성자, 제대로 한건지는 모르겠지만 Menu를 extends 하여서 거기에 있는 값 들을 super로 받아옴
     public Food (String foodName, String foodDesc, Double foodPrice) {
         // 이렇게 해서 일단 부모 클래스를 생성함
         super(foodName, foodDesc);
-        // 부모 클래스의 값을 가져와서 할당
+        // 부모 클래스의 값을 가져와서 할당 -> 없으면 null 값이 나옴 -> Main에 부모를 만들지 않고 해서 그런가?
         this.foodName = super.getMenuName();
         this.foodDesc = super.getMenuDesc();
         this.foodPrice = foodPrice;
